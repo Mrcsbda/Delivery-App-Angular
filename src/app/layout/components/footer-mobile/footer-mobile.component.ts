ff5586b3db21941c957df7eaa3c9747214723c8b
Mrcsbda <macbedoya@gmail.com>
2024-02-07 16:13:05 -0500

    :lipstick: footer mobile styles

--- src/app/layout/components/footer-mobile/footer-mobile.component.ts
+++ src/app/layout/components/footer-mobile/footer-mobile.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-footer-mobile',
+  selector: 'footer-mobile',
   templateUrl: './footer-mobile.component.html',
   styleUrl: './footer-mobile.component.scss'
 })
 export class FooterMobileComponent {
-
+  footerIcons = [
+    {
+      name: 'Home',
+      path: '../../../assets/images/home.svg'
+    },
+    {
+      name: 'Search',
+      path: '../../../assets/images/search.svg'
+    },
+    {
+      name: 'Orders',
+      path: '../../../assets/images/orders.svg'
+    },
+    {
+      name: 'Profile',
+      path: '../../../assets/images/profile.svg'
+    }
+  ]
 }
