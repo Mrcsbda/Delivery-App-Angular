ea33bda3b873800f6a2a3092c5ac33126dc75135
Mariana <macbedoya@gmail.com>
2024-02-20 17:16:44 -0500

    :lipstick: header desktop styles

--- src/app/layout/components/header-desktop/header-desktop.component.ts
+++ src/app/layout/components/header-desktop/header-desktop.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrl: './header-desktop.component.scss'
 })
 export class HeaderDesktopComponent {
+  userLogged: Boolean = false;
+  headerOptions = [
+    {
+      title: 'Home',
+      path: '/'
+    },
+    {
+      title: 'Search',
+      path: '/'
+    }
+  ]
 
+  headerOptionsUserLogged = [
+    {
+      title: 'Home',
+      path: '/'
+    },
+    {
+      title: 'Search',
+      path: '/'
+    },
+    {
+      title: 'Orders',
+      path: '/'
+    },
+    {
+      title: 'Profile',
+      path: '/'
+    }
+  ]
 }
