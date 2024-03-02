8b77f4562ad15569091e4a15d0339dedba589dd7
Mariana <macbedoya@gmail.com>
2024-03-02 12:30:42 -0500

    :lipstick: current orden component styles

--- src/app/layout/components/current-order/current-order.component.ts
+++ src/app/layout/components/current-order/current-order.component.ts
@@ -1,10 +1,62 @@
 import { Component } from '@angular/core';
 
+type OrderStatus = 'CONFIRMED' | 'COOKING' | 'ON THW WAY' | 'DELIVERED'
 @Component({
   selector: 'current-order',
   templateUrl: './current-order.component.html',
   styleUrl: './current-order.component.scss'
 })
 export class CurrentOrderComponent {
-
+  orderStep: number = 2
+  orderStatuses: OrderStatus[] = ['CONFIRMED', 'COOKING', 'ON THW WAY', 'DELIVERED']
+  ordersInfo = [
+    {
+      image: "https://res.cloudinary.com/dd3qzm4in/image/upload/v1692829899/deliveryApp/los%20perritos/Perro-Caliente_lqlgzk.jpg",
+      quantity: 1,
+      name: "Vegetarian pizza",
+      total: 32
+    },
+    {
+      image: "https://res.cloudinary.com/dd3qzm4in/image/upload/v1692829899/deliveryApp/los%20perritos/Perro-Caliente_lqlgzk.jpg",
+      quantity: 2,
+      name: "Fresh with funcheza asd sad asd as asdasd asdasd",
+      total: 28.45
+    },
+    {
+      image: "https://res.cloudinary.com/dd3qzm4in/image/upload/v1692829899/deliveryApp/los%20perritos/Perro-Caliente_lqlgzk.jpg",
+      quantity: 1,
+      name: "Vegetarian pizza",
+      total: 32
+    },
+    {
+      image: "https://res.cloudinary.com/dd3qzm4in/image/upload/v1692829899/deliveryApp/los%20perritos/Perro-Caliente_lqlgzk.jpg",
+      quantity: 2,
+      name: "Fresh with funcheza asd sad asd as asdasd asdasd",
+      total: 28.45
+    },
+    {
+      image: "https://res.cloudinary.com/dd3qzm4in/image/upload/v1692829899/deliveryApp/los%20perritos/Perro-Caliente_lqlgzk.jpg",
+      quantity: 1,
+      name: "Vegetarian pizza",
+      total: 32
+    },
+    {
+      image: "https://res.cloudinary.com/dd3qzm4in/image/upload/v1692829899/deliveryApp/los%20perritos/Perro-Caliente_lqlgzk.jpg",
+      quantity: 2,
+      name: "Fresh with funcheza asd sad asd as asdasd asdasd",
+      total: 28.45
+    },
+    {
+      image: "https://res.cloudinary.com/dd3qzm4in/image/upload/v1692829899/deliveryApp/los%20perritos/Perro-Caliente_lqlgzk.jpg",
+      quantity: 1,
+      name: "Vegetarian pizza",
+      total: 32
+    },
+    {
+      image: "https://res.cloudinary.com/dd3qzm4in/image/upload/v1692829899/deliveryApp/los%20perritos/Perro-Caliente_lqlgzk.jpg",
+      quantity: 2,
+      name: "Fresh with funcheza asd sad asd as asdasd asdasd",
+      total: 28.45
+    }
+  ]
 }
